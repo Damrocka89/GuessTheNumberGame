273c5f5670556f861781335cc7e24dfe2935657a
Ewelina Łodyńska <ew.krasowska@hotmail.com>
2019-06-27 11:34:02 +0200

    init

---
+++ core/src/main/java/academy/learnprogramming/MaxNumber.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Qualifier
+public @interface MaxNumber {
+
+
+}
